9d364d6006532ccb4fe9d5690d33fedc47c7d4b9
KaiFlame <42344215+KaiFlame@users.noreply.github.com>
2018-09-26 10:38:28 -0300

    Revert "Merge branch 'master' of https://github.com/Vinschers/Labirinto"
    This reverts commit 046067dbbe0b71830d4769a00b6c5d8c655fbb67, reversing
    changes made to c611cedda0cb2ef310ddf6cc602623ce48e4dbf4.

---
+++ Coordenada/Coordenada.java
@@ -0,0 +1,96 @@
+package Coordenada;
+public class Coordenada implements Cloneable{
+	protected int x, y;
+	/**
+	*	@param x É o número da linha da coordenada. É referente à posição vertical começa sempre no 0.
+	*	@param y É o número da coluna da coordenad. É referente à posição horizontal e começa sempre no 0.
+	*	@throws Exception quando o tamanho é menor ou igual que zero. O vetor não pode ter menos de 1 posição.
+	*/
+	public Coordenada(int x, int y) throws Exception{
+		if (x < 0)
+			throw new Exception("Valor do X invalido");
+		if (y < 0)
+			throw new Exception("Valor do Y invalido");
+		this.x = x;
+		this.y = y;
+	}
+	/**
+	* Método obrigatório
+	* @return uma string que contêm a coordenada x e y. Segue o padrão: (X, Y)
+	*/
+	public String toString(){
+		return "(" + this.x + ", " + this.y + ")";
+	}
+	/**
+	*	Método obrigatório.
+	*	@return Um boolean, sendo que quando verdadeiro, a coordenada passada como parâmetro é igual a this e, quando falso, são diferentes.
+	*	@param obj é o objeto a ser comparado com this.
+	*/
+	public boolean equals(Object obj){
+		if (obj == null)
+			return false;
+		if (obj == this)
+			return true;
+		if (obj.getClass() != this.getClass())
+			return false;
+		Coordenada c = (Coordenada)obj;
+		if (c.x != this.x)
+			return false;
+		if (c.y != this.y)
+			return false;
+		return true;
+	}
+	/**
+	*	Método obrigatório hashCode().
+	*	@return o hashCode de this.
+	*/
+	public int hashCode(){
+		int ret = 1;
+		ret = ret * 2 + new Integer(this.x).hashCode();
+		ret = ret * 2 + new Integer(this.y).hashCode();
+		return ret;
+	}
+	/**
+	*	Método Obrigatório clone(). Copia a Coordenada atual (this) através do construtor de cópia
+	*	@return um objeto que é exatamente igual à this.
+	*/
+	public Object clone(){
+		Coordenada coord = null;
+		try {
+			coord = new Coordenada(this);
+		}
+		catch (Exception e) {}
+		return coord;
+	}
+	/**
+	*	Método que copia uma coordenada para this. Com isso, this será exatamente igual à coordenada a ser copiada.
+	*	@param c é uma coordenada que será copiada para a coordenada atual (this).
+	*	@throws Exception quando a coordenada que seria copiada é null.
+	*/
+	public Coordenada(Coordenada c) throws Exception{
+		if (c == null)
+			throw new Exception("Objeto é inválido!");
+		this.x = c.x;
+		this.y = c.y;
+	}
+	/**
+	*	Método que retorna o x da coordenada. O x é a linha da coordenada e é referente à vertical da matriz.
+	*	@throws Exception quando a linha é menor que 0.
+	*	@return a linha x da coordenada
+	*/
+	public int getX() throws Exception{
+		if (this.x < 0)
+			throw new Exception("x era menor que 0!");
+		return this.x;
+	}
+	/**
+	*	Método que retorna o y da coordenada. O y é a coluna da coordenada e é referente à horizontal da matriz.
+	*	@throws Exception quando a coluna é menor que 0.
+	*	@return a coluna y da coordenada
+	*/
+	public int getY() throws Exception{
+		if (this.y < 0)
+			throw new Exception("y era menor que 0!");
+		return this.y;
+	}
+}
---
+++ Fila/Fila.java
@@ -0,0 +1,189 @@
+package Fila;
+import java.lang.reflect.*;
+ //FIFO = First In First Out
+ public class Fila<X> implements Cloneable{
+ 	protected int qtd, comeco, fim;
+ 	protected Object[] vetor;
+
+	/**
+	*	Método que serve para clonar o x, ou objeto genérico. Sem o método meuCloneDeX(), o programa acusa erro, já que, embora nós, programadores,
+	*	tenhamos certeza que x é cloneable, o programa não sabe. O método então força o .clone().
+	*	@return O clone do objeto da classe genérica. Ele é usado para acessar o .clone() após o programa ter certeza que a classe genérica é cloneable.
+	*	@param x a instânica do objeto é passada para ser clonada.
+	*/
+	protected X meuCloneDeX(X x)
+    {
+        X ret = null;
+        try{
+            Class<?> classe = x.getClass();
+            Class<?>[] tiposDoParametroFormal;
+            tiposDoParametroFormal = null;
+            Method metodo = classe.getMethod("clone", tiposDoParametroFormal);
+            Object[] parametrosReais = null;
+            ret = (X)metodo.invoke(x, parametrosReais);
+        }
+        catch(InvocationTargetException erro){}
+        catch(NoSuchMethodException erro2){}
+        catch(IllegalAccessException erro3){}
+        return ret;
+    }
+	/**
+	*	@param tamanho É o tamanho da fila específicado. O vetor dentro da classe têm propriedade .lenght igualada ao tamanho.
+	*	@throws Exception quando o tamanho é menor ou igual que zero. O vetor não pode ter menos de 1 posição.
+	*/
+ 	public Fila(int tamanho) throws Exception{
+ 		if (tamanho <= 0)
+ 			throw new Exception("Tamanho invalido");
+ 		this.vetor = new Object[tamanho];
+ 		this.qtd = 0;
+ 		this.comeco = 0;
+ 		this.fim = 0;
+ 	}
+	/**
+	*	Método que copia uma fila para this. Com isso, this será exatamente igual à fila a ser copiada.
+	*	@param modelo é uma fila que será copiada para a fila atual.
+	*	@throws Exception quando a fila que seria copiada é null.
+	*/
+ 	public Fila(Fila<X> modelo) throws Exception {
+ 		if (modelo == null)
+ 			throw new Exception("Modelo invalido");
+ 		this.qtd = modelo.qtd;
+ 		this.comeco = modelo.comeco;
+ 		this.fim = modelo.fim;
+ 		this.vetor = new Object[modelo.vetor.length];
+ 		for (int i = 0; i < vetor.length; i++) {
+ 			this.vetor[i] = modelo.vetor[i];
+ 		}
+ 	}
+	/**
+	* Método obrigatório
+	* @return uma string que contêm o número de itens guardados na fila e o primeiro deles.
+	*/
+ 	public String toString() {
+ 		if (this.qtd == 0)
+ 			return "Vazia";
+ 		return this.qtd + "elementos, sendo o primeiro " + this.vetor[this.comeco].toString();
+ 	}
+	/**
+	*	Método obrigatório.
+	*	@return Um boolean, sendo que quando verdadeiro, a fila passada como parâmetro é igual a this e, quando falso, são diferentes.
+	*	@param obj é o objeto a ser comparado com this.
+	*/
+ 	public boolean equals(Object obj) {
+ 		if (this == obj)
+ 			return true;
+ 		if (obj == null)
+ 			return false;
+ 		if (this.getClass() != obj.getClass())
+ 			return false;
+ 		Fila<X> f = (Fila<X>)obj;
+ 		if (this.qtd != f.qtd)
+ 			return false;
+ 		for (int i = 0, posThis = this.comeco, posFila = f.comeco; i < this.qtd;
+ 		i++, posThis = (posThis<this.vetor.length-1?posThis+1:0),
+ 		posFila = (posFila<f.vetor.length-1?posFila+1:0)) {
+ 			if (!this.vetor[posThis].equals(f.vetor[posFila]))
+ 				return false;
+ 		}
+ 		return true;
+ 	}
+	/**
+	*	Método Obrigatório clone(). Copia a Fila atual (this) através do construtor de cópia
+	*	@return um objeto que é exatamente igual à this.
+	*/
+ 	public Object clone() {
+ 		Fila ret = null;
+ 		try {
+ 			ret = new Fila<X>(this);
+ 		}
+ 		catch (Exception ex) {}
+ 		return ret;
+ 	}
+	/**
+	*	Método obrigatório hashCode().
+	*	@return o hashCode de this.
+	*/
+ 	public int hashCode() {
+ 		int ret = 3;
+ 		ret = ret * 3 + new Integer(this.qtd).hashCode();
+ 		ret = ret * 3 + new Integer(this.fim).hashCode();
+ 		ret = ret * 3 + new Integer(this.comeco).hashCode();
+ 		for (int i = 0, pos = comeco; i < this.qtd; i++, pos=((pos<vetor.length)?pos+1:0)) {
+			if (this.vetor[pos] != null)
+ 				ret = ret * 2 + this.vetor[pos].hashCode();
+ 		}
+ 		return ret;
+ 	}
+	/**
+	*	Método que devolve um boolean que diz se a fila está cheia ou não.
+	*	@return boolean de comparação com o tamanho do vetor da fila. Se a quantidade de itens na fila foi igual ao tamanho do vetor, retorna true.
+	*/
+ 	public boolean isCheia() {
+ 		return this.qtd == this.vetor.length;
+ 	}
+	/**
+	*	Método que devolve um boolean que diz se a fila está vazia ou não.
+	*	@return boolean de comparação com a quantidade de itens da fila. Se a quantidade de itens na fila foi igual à 0, retorna true.
+	*/
+ 	public boolean isVazia() {
+ 		return this.qtd == 0;
+ 	}
+	/**
+	*	Método que guarda coisas na fila. Ele guarda sempre no final + 1 da fila.
+	*	@throws Exception quando o que for ser guardado é null ou quando a fila já está cheia.
+	*	@param x é o objeto que será guardado.
+	*/
+ 	public void guarde(X x) throws Exception{
+ 		if (x == null)
+ 			throw new Exception("Instancia invalida");
+ 		if (this.isCheia())
+ 			throw new Exception("Sem espaco de armazenamento");
+ 		if (x instanceof Cloneable) {
+			if (this.fim == this.vetor.length - 1) {
+				this.fim = 0;
+				this.vetor[this.fim] = meuCloneDeX(x);
+			}
+			else
+				this.vetor[this.fim++] = meuCloneDeX(x);
+		}
+ 		else {
+			if (this.fim == this.vetor.length - 1) {
+				this.fim = 0;
+				this.vetor[this.fim] = x;
+			}
+			else
+				this.vetor[this.fim++] = x;
+		}
+ 		this.qtd++;
+ 	}
+	/**
+	*	Método que recupera o primeiro item da fila. Ele clona o objeto armazenado e devolve o objeto clonado.
+	*	@throws Exception quando não houver nada na fila.
+	*	@return o item clonado referente a primeira posição do vetor.
+	*/
+ 	public X getUmItem() throws Exception{
+ 		if (this.isVazia()) {
+ 			throw new Exception("Armazenamento vazio");
+ 		}
+ 		if (this.vetor[comeco] instanceof Cloneable) {
+ 			return meuCloneDeX((X)this.vetor[this.comeco]);
+		}
+ 		else
+ 			return (X)this.vetor[this.comeco];
+ 	}
+	/**
+	*	Método que exclui um item da fila. É sempre o primeiro o item a ser excluído.
+	*	@throws Exception quando a fila já está vazia. É impossível excluir coisas de algo vazio.
+	*/
+ 	public void jogueForaUmItem() throws Exception {
+ 		if (this.isVazia()) {
+ 			throw new Exception("Nao ha nada para excluir");
+ 		}
+ 		this.vetor[this.comeco] = null;
+ 		if (this.comeco == this.vetor.length - 1)
+ 			this.comeco = 0;
+ 		else
+ 			this.comeco++;
+ 		this.qtd--;
+ 	}
+}
